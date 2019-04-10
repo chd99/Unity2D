128c5c76361b26fc5109851d2f0c18a245a55cda
chd99 <chendi99@163.com>
2019-04-10 12:12:51 +0800

    Block Breaker init commit

---
+++ Block_Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int breakableBlocks;  //Serialized for debugging purpose
+
+    //cached reference
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBreakableBlocks()
+    {
+            breakableBlocks++;
+    }
+
+    public void BlockDestoryed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
---
+++ Block_Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthUnits = 16f;
+    [SerializeField] float minPaddleX;
+    [SerializeField] float maxPaddleX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mouse postion by Units, for transform.postison
+        float mousePosInUnits  = Input.mousePosition.x / Screen.width * screenWidthUnits;
+
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minPaddleX, maxPaddleX);
+
+        transform.position = paddlePos;
+    }
+}
