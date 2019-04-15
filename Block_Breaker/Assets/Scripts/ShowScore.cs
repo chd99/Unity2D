d6e43e8e735ff19373321a5b2c1ec7ec4f4a2523
chd99 <chendi99@163.com>
2019-04-15 17:29:44 +0800

    Add ShowScore Script

---
+++ Block_Breaker/Assets/Scripts/ShowScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowScore : MonoBehaviour
+{
+    // config params
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    GameSession theGameSession;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theGameSession = FindObjectOfType<GameSession>();
+        scoreText.text = theGameSession.GetScore().ToString();
+    }
+}
